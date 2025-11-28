8a54b18454126b96df4d2d5c18b697e961b784cc
Proky0 <astril79@gmail.com>
2025-11-28 22:38:32 +0100

    Update: format

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
-author 'Proky'
-description 'Ped Preview'
-version '1.0.0'
+author "Proky"
+description "Ped Preview"
+version "1.0.0"
 
-client_script 'client/client.lua'
+client_script "client/client.lua"
